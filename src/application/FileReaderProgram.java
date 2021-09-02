e626b0293b6152f30c5bb286347df2a3133b06c5
Jairo Dias <jairopereira_dias@hotmail.com>
2021-09-02 18:06:42 -0300

    BufferedWrite with FileWriter

--- src/application/FileReaderProgram.java
+++ src/application/FileReaderProgram.java
@@ -1,23 +1,24 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReaderProgram {
     public static void main(String[] args) {
-        String path = "c:\\tmp\\testdox.txt";
+        String[] lines = new String[] { "Good morning", "Good afternoon" };
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
+        String path = "c:\\tmp\\out.txt";
 
-            while (line != null) {
-                System.out.println(line);
-
-                line = br.readLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
